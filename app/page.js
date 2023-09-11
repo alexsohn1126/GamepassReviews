291f148ccf58f49ac2f7aa6b6f3b8847952cda7a
Moohaeng Sohn <alexsohn1126@gmail.com>
2023-09-11 16:02:52 -0400

    Enabled TailwindCSS, prototyping game component

--- app/page.js
+++ app/page.js
@@ -3,7 +3,23 @@
 import next from 'next';
 import { useState } from 'react';
 
-export default function Main() {
-  var json = require('../public/testfile.json');
+function GameDisplay({json}) {
   console.log(json);
+  return (
+    <div class="w-400">
+      <img class="rounded" src="{json.Images[0].Uri}"></img>
+      <h3 class="text-gray-300">{json.ProductTitle}</h3>
+      <h4 class="text-gray-300">{json.DeveloperName}</h4>
+      <h4 class="text-gray-300"><i>{json.PublisherName}</i></h4>
+    </div>
+  )
+}
+
+export default function Main() {
+  const json = require('../public/testfile.json');
+  //console.log(json);
+
+  return (
+    <GameDisplay json={json}></GameDisplay>
+  )
 }
